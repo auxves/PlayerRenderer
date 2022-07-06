a9a7377335a5354856f7b6b002ed7571f4d2e527
Glossnyx <arnohovhannisyan0@gmail.com>
2022-07-05 21:40:16 -0700

    add blinking and clean up code

--- Frame.cs
+++ Frame.cs
@@ -2,25 +2,42 @@
 
 namespace PlayerRenderer;
 
-public abstract class Frame
+public abstract record Frame
 {
-	public virtual void Setup(Player player) {}
-}
+	public virtual void Setup(Player player) { }
+	public virtual void Cleanup(Player player) { }
 
-public sealed class PoseFrame : Frame
-{
-	public static readonly PoseFrame Default = new() { HeadFrame = 0, BodyFrame = 0, LegFrame = 0 };
-
-	public int HeadFrame { get; init; }
-	public int BodyFrame { get; init; }
-	public int LegFrame { get; init; }
-
-	public override void Setup(Player player)
+	public sealed record Pose(int HeadFrame, int BodyFrame, int LegFrame) : Frame
 	{
-		player.headFrame.Y = player.headFrame.Height * HeadFrame;
-		player.bodyFrame.Y = player.bodyFrame.Height * BodyFrame;
-		player.legFrame.Y = player.legFrame.Height * LegFrame;
+		public Pose(int Frame) : this(Frame, Frame, Frame) { }
+
+		public static readonly Pose Default = new(0);
+
+		public override void Setup(Player player)
+		{
+			player.headFrame.Y = player.headFrame.Height * HeadFrame;
+			player.bodyFrame.Y = player.bodyFrame.Height * BodyFrame;
+			player.legFrame.Y = player.legFrame.Height * LegFrame;
+		}
+	}
+
+	public sealed record Sitting : Frame { }
+
+	public sealed record Blinking : Frame
+	{
+		public override void Setup(Player player)
+		{
+			Pose.Default.Setup(player);
+			player.eyeHelper.BlinkBecausePlayerGotHurt();
+			player.eyeHelper.Update(player);
+		}
+
+		public override void Cleanup(Player player)
+		{
+			for (int i = 0; i < 20; i++)
+			{
+				player.eyeHelper.Update(player);
+			}
+		}
 	}
 }
-
-public sealed class SittingFrame : Frame {}
--- PlayerRenderer.cs
+++ PlayerRenderer.cs
@@ -8,6 +8,8 @@ using System;
 
 namespace PlayerRenderer;
 
+using static Frame;
+
 public class PlayerRenderer : Mod
 {
 	public const int WIDTH = 44;
@@ -23,80 +25,74 @@ public class PlayerRenderer : Mod
 
 	public static readonly Frame[] Frames =
 	{
-		PoseFrame.Default,
-		new PoseFrame { HeadFrame = 5, BodyFrame = 6, LegFrame = 5 },
-		new PoseFrame { HeadFrame = 6, BodyFrame = 6, LegFrame = 6 },
-		new PoseFrame { HeadFrame = 7, BodyFrame = 7, LegFrame = 7 },
-		new PoseFrame { HeadFrame = 8, BodyFrame = 8, LegFrame = 8 },
-		new PoseFrame { HeadFrame = 9, BodyFrame = 9, LegFrame = 9 },
-		new PoseFrame { HeadFrame = 10, BodyFrame = 10, LegFrame = 10 },
-		new PoseFrame { HeadFrame = 11, BodyFrame = 11, LegFrame = 11 },
-		new PoseFrame { HeadFrame = 12, BodyFrame = 12, LegFrame = 12 },
-		new PoseFrame { HeadFrame = 13, BodyFrame = 13, LegFrame = 13 },
-		new PoseFrame { HeadFrame = 14, BodyFrame = 14, LegFrame = 14 },
-		new PoseFrame { HeadFrame = 15, BodyFrame = 15, LegFrame = 15 },
-		new PoseFrame { HeadFrame = 16, BodyFrame = 16, LegFrame = 16 },
-		new PoseFrame { HeadFrame = 17, BodyFrame = 17, LegFrame = 17 },
-		new PoseFrame { HeadFrame = 18, BodyFrame = 18, LegFrame = 18 },
-		new PoseFrame { HeadFrame = 19, BodyFrame = 19, LegFrame = 19 },
-		PoseFrame.Default,
-		PoseFrame.Default,
-		new SittingFrame(),
-		PoseFrame.Default,
-		PoseFrame.Default,
+		Pose.Default,
+		new Pose(5, 6, 5),
+		new Pose(6),
+		new Pose(7),
+		new Pose(8),
+		new Pose(9),
+		new Pose(10),
+		new Pose(11),
+		new Pose(12),
+		new Pose(13),
+		new Pose(14),
+		new Pose(15),
+		new Pose(16),
+		new Pose(17),
+		new Pose(18),
+		new Pose(19),
+		Pose.Default,
+		Pose.Default,
+		new Sitting(),
+		Pose.Default,
+		new Blinking(),
 	};
 
-	public static void Render(Player player, string name)
+	public static void Render(Player player, string path)
 	{
 		RenderingSpritesheet = true;
 
-		using var stream = new RenderStream(Main.spriteBatch.GraphicsDevice, WIDTH, HEIGHT * FRAME_COUNT) { OutputName = $"{name}.png" };
+		using var stream = new RenderStream(Main.spriteBatch.GraphicsDevice, WIDTH, HEIGHT * FRAME_COUNT) { OutputPath = path };
 
 		for (FrameIndex = 0; FrameIndex < FRAME_COUNT; FrameIndex++)
 		{
-			Frames[FrameIndex].Setup(player);
+			var frame = Frames[FrameIndex];
+			frame.Setup(player);
 			Main.PlayerRenderer.DrawPlayers(Main.Camera, new[] { player });
+			frame.Cleanup(player);
 		}
 
 		RenderingSpritesheet = false;
 	}
 
-	public static void RenderHead(Player player, string name)
+	public static void RenderHead(Player player, string path)
 	{
-		using var stream = new RenderStream(Main.spriteBatch.GraphicsDevice, HEAD_SIZE, HEAD_SIZE) { OutputName = $"{name}_Head.png" };
+		using var stream = new RenderStream(Main.spriteBatch.GraphicsDevice, HEAD_SIZE, HEAD_SIZE) { OutputPath = path };
 
-		Main.spriteBatch.Begin(
-			(SpriteSortMode)1,
-			BlendState.AlphaBlend,
-			Main.Camera.Sampler,
-			DepthStencilState.None,
-			Main.Camera.Rasterizer,
-			null,
-			Main.Camera.GameViewMatrix.TransformationMatrix
-		);
-
+		Main.spriteBatch.Begin();
 		Main.PlayerRenderer.DrawPlayerHead(Main.Camera, player, new Vector2(stream.Width / 2 - 4, stream.Height / 2 - 4));
-
 		Main.spriteBatch.End();
 	}
 }
 
-public class PositionMover : ModPlayer
+public class DrawInfoModifier : ModPlayer
 {
 	public override void ModifyDrawInfo(ref PlayerDrawSet drawInfo)
 	{
 		if (!PlayerRenderer.RenderingSpritesheet) return;
 
+		var frame = PlayerRenderer.Frames[PlayerRenderer.FrameIndex];
+
 		drawInfo.Position = Main.screenPosition + new Vector2(PlayerRenderer.WIDTH / 4, 8 + PlayerRenderer.FrameIndex * PlayerRenderer.HEIGHT);
-		drawInfo.isSitting = PlayerRenderer.Frames[PlayerRenderer.FrameIndex] is SittingFrame;
+		drawInfo.isSitting = frame is Sitting;
 	}
 }
 
 public class RenderStream : IDisposable
 {
-	public string OutputName { get; init; }
+	public string OutputPath { get; init; }
 
-	public RenderTarget2D Target { get; init; }
+	public RenderTarget2D Target { get; private init; }
 	public int Width => Target.Width;
 	public int Height => Target.Height;
 
@@ -110,11 +106,8 @@ public class RenderStream : IDisposable
 
 	public void Dispose()
 	{
-		Directory.CreateDirectory(PlayerRenderer.SavePath);
+		using var stream = File.Create(OutputPath);
 
-		var path = Path.Combine(PlayerRenderer.SavePath, OutputName);
-		using var stream = File.Create(path);
-		
 		Target.SaveAsPng(stream, Width, Height);
 		Target.Dispose();
 
@@ -134,24 +127,29 @@ public class RenderCommand : ModCommand
 	{
 		if (Main.GameZoomTarget != 1f)
 		{
-			caller.Reply($"Game Zoom must be 100% for the sprites to render properly", Color.Red);
-			caller.Reply($"If the slider doesn't go down to 100%, disable Forced Zoom in the tModLoader Settings", Color.LightSalmon);
+			caller.Reply("Game Zoom must be 100% for the sprites to render properly", Color.OrangeRed);
+			caller.Reply("If the slider doesn't go down to 100%, disable Forced Zoom in the tModLoader Settings", Color.LightSalmon);
 			return;
 		}
 
 		if (args.Length == 0)
 		{
-			caller.Reply("Must provide <name> to the render command", Color.Red);
+			caller.Reply("Must provide <name> to the render command", Color.OrangeRed);
 			return;
 		}
 
 		var name = args[0];
 
-		PlayerRenderer.Render(caller.Player, name);
-		PlayerRenderer.RenderHead(caller.Player, name);
+		Directory.CreateDirectory(PlayerRenderer.SavePath);
 
-		caller.Reply($"Spritesheet saved to {Path.Combine(PlayerRenderer.SavePath, $"{name}.png")}", Color.LightBlue);
-		caller.Reply($"Head saved to {Path.Combine(PlayerRenderer.SavePath, $"{name}_Head.png")}", Color.LightBlue);
+		var spritesheetPath = Path.Combine(PlayerRenderer.SavePath, $"{name}.png");
+		var headPath = Path.Combine(PlayerRenderer.SavePath, $"{name}_Head.png");
+
+		PlayerRenderer.Render(caller.Player, spritesheetPath);
+		PlayerRenderer.RenderHead(caller.Player, headPath);
+
+		caller.Reply($"Spritesheet saved to {spritesheetPath}", Color.LightBlue);
+		caller.Reply($"Head saved to {headPath}", Color.LightBlue);
 		caller.Reply("The Spritesheet should be a drop-in replacement for a Town NPC Sprite, but you should probably crop the Head sprite manually", Color.LightSalmon);
 	}
 }
